5a537079a3c2a3f7443ca77ec8c4b4b21ee05f7a
sayakpaul <spsayakpaul@gmail.com>
2024-09-11 12:04:37 +0530

    make it a package.

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import find_packages, setup
+
+
+setup(
+    name="wmdetection",
+    version="0.1.0",
+    description="Simple watermark detection.",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=["torch", "numpy"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved ::Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
